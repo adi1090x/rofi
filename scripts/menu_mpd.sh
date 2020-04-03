bcc5b8e0481bf8a83517d230d79d0772711d6ea4
adi1090x <adi1090x@gmail.com>
2020-04-04 03:36:49 +0530

    Updated

--- scripts/menu_mpd.sh
+++ scripts/menu_mpd.sh
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
 rofi_command="rofi -theme themes/menu/mpd.rasi"
 
--- scripts/mpd.sh
+++ scripts/mpd.sh
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
 rofi_command="rofi -theme themes/mpd.rasi"
 
