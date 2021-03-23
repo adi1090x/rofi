ad991683134c6a5352426129ab839bc30175b137
GroosL <59540625+GroosL@users.noreply.github.com>
2021-03-23 09:25:22 -0300

    Update cmus.sh

--- 720p/applets/applets/cmus.sh
+++ 720p/applets/applets/cmus.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
 style="$($HOME/.config/rofi/applets/applets/style.sh)"
 
 dir="$HOME/.config/rofi/applets/applets/configs/$style"
