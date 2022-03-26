80bde7dfe2f7a9bb0dc0d657759f976a25f1afe9
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-03-26 21:02:12 +0100

    [powermenu] add quit for awesome window manager
    quit awesome window manager using its client tool

--- 1080p/applets/android/powermenu.sh
+++ 1080p/applets/android/powermenu.sh
@@ -84,6 +84,8 @@ case $chosen in
 				bspc quit
 			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
 				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == "awesome" ]]; then
+				awesome-client "awesome.quit()"
 			fi
 		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
 			exit 0
