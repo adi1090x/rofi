1aa09b66e21e9639d2449baa8155a874a395af73
adi1090x <adi1090x@gmail.com>
2022-08-13 17:00:40 +0530

    Added type-4 launchers

---
+++ files/launchers/type-4/launcher.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
+#
+## Available Styles
+#
+## style-1     style-2     style-3     style-4     style-5
+## style-6     style-7     style-8     style-9     style-10
+
+dir="$HOME/.config/rofi/launchers/type-4"
+theme='style-1'
+
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}/${theme}.rasi
