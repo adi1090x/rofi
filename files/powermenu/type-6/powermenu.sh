361e7fdf0042804ddbb7e2ad9817bfd71cebc341
Uriel MM <moyauriel05@gmail.com>
2022-08-25 18:13:26 -0500

    Add loguot option for qtile WM

--- files/powermenu/type-6/powermenu.sh
+++ files/powermenu/type-6/powermenu.sh
@@ -82,6 +82,8 @@ run_cmd() {
 				i3-msg exit
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			elif [[ "$DESKTOP_SESSION" == 'qtile' ]]; then
+               qtile cmd-obj -o cmd -f shutdown 
 			fi
 		fi
 	else
