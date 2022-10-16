c8108fe9ad3394d0a6f5a59ca3231b1705174481
VDuchauffour <vincent.duchauffour@proton.me>
2022-10-16 13:56:12 +0200

    Add xsel case for session locking

--- files/powermenu/type-1/powermenu.sh
+++ files/powermenu/type-1/powermenu.sh
@@ -99,6 +99,8 @@ case ${chosen} in
 			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
+		elif [[ -x '/usr/bin/xsel' ]]; then
+      xset s activate
 		fi
         ;;
     $suspend)
